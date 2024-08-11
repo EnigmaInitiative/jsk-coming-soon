7b96db6f9ddc3be3d8ff8b1c81e187cac1942c47
NishantGupt786 <nishantgupta2325@gmail.com>
2024-08-11 11:26:05 +0530

    sheets integration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const nextConfig = {
-    // async rewrites() {
-    //   return [
-    //     {
-    //         source: '/api/sheets',
-    //         destination: ``,
-    //     },
-    //   ];
-    // }
+    async rewrites() {
+      return [
+        {
+            source: '/api/sheets',
+            destination: `https://script.google.com/macros/s/AKfycbygJwcU5rfQ1fLXklj75Y_N1vFeg1wpFn35m7A4hvxzNP6K-XS8siriotVNHj4iXG80sA/exec`,
+        },
+      ];
+    }
   };
   
   export default nextConfig;
